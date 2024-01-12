ac5873c53b32ba69d5252497c15cd56657075adf
IvoGeorgievx <102153817+IvoGeorgievx@users.noreply.github.com>
2024-01-12 20:01:32 +0200

    playground

--- playground-fe/src/app/main/main.module.ts
+++ playground-fe/src/app/main/main.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TodosComponent } from './todos/todos.component';
-import { CreateTodoComponent } from './create-todo/create-todo.component';
 import { ReactiveFormsModule } from '@angular/forms';
+
 import { HomeComponent } from './home/home.component';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+
+import { CreateTodoComponent } from './create-todo/create-todo.component';
+import { TodosComponent } from './todos/todos.component';
 
 @NgModule({
   declarations: [TodosComponent, CreateTodoComponent, HomeComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, ButtonModule, TableModule],
   exports: [TodosComponent, CreateTodoComponent, HomeComponent],
 })
 export class MainModule {}
