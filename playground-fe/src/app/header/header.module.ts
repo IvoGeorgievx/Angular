a88358b5da905bbd47fd5925ab7e25140d23575c
IvoGeorgievx <102153817+IvoGeorgievx@users.noreply.github.com>
2024-01-21 16:59:20 +0200

    playground

--- playground-fe/src/app/header/header.module.ts
+++ playground-fe/src/app/header/header.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './login/login.component';
 import { CurrentUserComponent } from './current-user/current-user.component';
 import { LogoutComponent } from './logout/logout.component';
 import { RouterModule } from '@angular/router';
+import {IonicModule} from "@ionic/angular";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { RouterModule } from '@angular/router';
     CurrentUserComponent,
     LogoutComponent,
   ],
-  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule, IonicModule],
   exports: [
     NavComponent,
     RegistrationComponent,
